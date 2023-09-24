2958ff3c4fa745c1c082f4ac89b63aa5dcc226a4
Shamari Lewis <superskl5000@gmail.com>
2023-09-24 16:29:28 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ const exphbs = engine;
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) =>{
-    res.status(200).render("home", {msg: "Handlebars are Cool!"});
+
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II"},
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+app.get("/", (req, res) => {
+    res.render("reviews-index", { reviews: reviews });
 })
+
+// app.get("/", (req, res) =>{
+//     res.status(200).render("home", {msg: "Handlebars are Cool!"});
+// })
 
 app.listen(8080, () => {
     console.log("App is listening on Port 8080")
