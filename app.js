47558e923bf1e8f070cada4005efd99716c133c9
Shamari Lewis <superskl5000@gmail.com>
2023-09-24 18:36:01 -0700

    Users can create reviews

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const app = express();
 let { engine } = require("express-handlebars");
 const exphbs = engine;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// This line must appear after the app = express() declaration.
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 mongoose.connect("mongodb+srv://superskl5000:BE5IorpNY0Z6ApoV@100devs.c7wjvtf.mongodb.net/rotten-potatoes?retryWrites=true&w=majority", { useNewUrlParser: true});
 
 const Review = new mongoose.model("Review", {
     title: String,
-    movieTitle: String
+    description: String,
+    movieTitle: String,
 });
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
@@ -29,6 +34,24 @@ app.get("/", async(req, res) => {
         console.log(error);
     }
     
+})
+
+app.get("/reviews/new", async(req, res) => {
+    try {
+        res.render("reviews-new", {});
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.post("/reviews", async(req, res) => {
+    try {
+        const review = Review.create(req.body);
+        console.log(review);
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 // app.get("/", (req, res) =>{
