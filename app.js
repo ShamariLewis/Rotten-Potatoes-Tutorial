b9cf2f61d7e1a6d9adde1c7bb5270ebdc4936f67
Shamari Lewis <superskl5000@gmail.com>
2023-09-24 18:16:01 -0700

    Added MongoDB and Review Model

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ const express = require("express");
 const app = express();
 let { engine } = require("express-handlebars");
 const exphbs = engine;
+const mongoose = require("mongoose");
+
+
+mongoose.connect("mongodb+srv://superskl5000:BE5IorpNY0Z6ApoV@100devs.c7wjvtf.mongodb.net/rotten-potatoes?retryWrites=true&w=majority", { useNewUrlParser: true});
+
+const Review = new mongoose.model("Review", {
+    title: String,
+    movieTitle: String
+});
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman II"},
-    { title: "Awesome Movie", movieTitle: "Titanic" }
-]
+// let reviews = [
+//     { title: "Great Review", movieTitle: "Batman II", releaseYear: 2012},
+//     { title: "Awesome Movie", movieTitle: "Titanic", releaseYear: 2000 }
+// ]
 
-app.get("/", (req, res) => {
-    res.render("reviews-index", { reviews: reviews });
+app.get("/", async(req, res) => {
+    try {
+        const reviews = Review.find()
+        res.render("reviews-index", { reviews: reviews });
+    } catch (error) {
+        console.log(error);
+    }
+    
 })
 
 // app.get("/", (req, res) =>{
